ba9a5fa2ca0c3c6f6c9cc6ca78b834a111aab0e6
Thomas Cazort <thomas.cazort@gmail.com>
2023-07-06 07:53:21 -0500

    Added desc

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Thomas Cazort Portfolio';
+export const SITE_DESCRIPTION = 'Thomas Cazort - Full Stack Engineer';
