8a04ee3231eb772967a22df31e23e985357d4f35
Thomas Cazort <thomas.cazort@gmail.com>
2023-11-19 12:42:53 -0800

    New Style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,29 +4,31 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
-		themes: ["dark", 'business', { mytheme: {
-			
-   "primary": "#1d4ed8",
-			
-   "secondary": "#3b82f6",
-			
-   "accent": "#1fb2a6",
-			
-   "neutral": "#2a323c",
-			
-   "base-100": "#1d232a",
-			
-   "info": "#3abff8",
-			
-   "success": "#36d399",
-			
-   "warning": "#fbbd23",
-			
-   "error": "#f87272",
-			},}], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: ["dark", 'business', {
+			mytheme: {
+
+				"primary": "#835EED",
+
+				"secondary": "#282433",
+
+				"accent": "#1fb2a6",
+
+				"neutral": "#262626",
+
+				"base-100": "#1e1e1e",
+
+				"info": "#3abff8",
+
+				"success": "#36d399",
+
+				"warning": "#fbbd23",
+
+				"error": "#f87272",
+			},
+		}], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "mytheme", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+	}
 }
