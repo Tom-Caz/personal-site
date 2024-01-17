6b70dac9b7ecb8e5f04a31601a731ea234674ea8
Thomas Cazort <thomas.cazort@gmail.com>
2024-01-16 23:01:23 -0800

    removed vercel

--- .vercel/output/static/pages/404.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('../chunks/pages/404_q9Glm0gr.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/static/pages/index.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('../chunks/pages/index_qAP3V3E3.mjs');
-
-export { page };
