288bbe3d4aba5d996c958dd21ed28d15038a6126
Thomas Cazort <thomas.cazort@gmail.com>
2024-01-16 22:51:39 -0800

    swap to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thomascazort.dev',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "static",
+  output: "server",
   adapter: vercel({ webAnalytics: { enabled: true } })
 });
