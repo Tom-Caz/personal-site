2643135929451cc85bc36278ff3032abd2013a1e
Thomas Cazort <thomas.cazort@gmail.com>
2024-02-22 14:43:44 -0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thomascazort.dev',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "static",
-  adapter: vercel({ webAnalytics: true })
+  output: "server",
+  adapter: vercel({ analytics: true })
 });
